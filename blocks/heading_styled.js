c386db5f18838e0328e3a8aaa23fe2a343fdfd0e
merrik <frenkdave@gmail.com>
2015-10-11 02:58:03 +0300

    Add headings with h1, h2, h3 options

---
+++ blocks/heading_styled.js
@@ -0,0 +1,9 @@
+/**
+ * Headings with different sizes
+ */
+SirTrevor.Blocks.HeadingStyled = SirTrevor.Blocks.Heading.extend({
+
+    type: "heading_styled",
+
+    blockOptions: [{text: 'h1', value: 'h1'},{text: 'h2', value: 'h2', default: true},{text: 'h3', value: 'h3'}]
+});
