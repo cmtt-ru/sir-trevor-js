1716f9e64e4ee38de56d0e8c84f438b9dab6cb65
merrik <frenkdave@gmail.com>
2015-10-11 20:32:03 +0300

    add deletable mixin (called undeletable for now)

---
+++ blocks/link-embed.js
@@ -0,0 +1,3 @@
+/**
+ * Created by davidf on 11.10.15.
+ */
---
+++ src/block_mixins/undeletable.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _ = require('../lodash');
+var config = require('../config');
+var utils = require('../utils');
+
+module.exports = {
+
+  mixinName: "Undeletable",
+
+  onDeleteClick: false,
+
+
+};
